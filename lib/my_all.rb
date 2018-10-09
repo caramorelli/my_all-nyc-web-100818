86b33f56f12411cdbec8378af64baa3381f6df4d
Cara Morelli <caramorelli7@gmail.com>
2018-10-09 12:59:12 +0000

    automatically backed up by learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -3,11 +3,15 @@ require 'pry'
 def my_all?(collection)
   idx = 0 
   while idx < collection.size 
+    ele = collection[idx]
     return false unless yield(collection[idx]) 
     idx += 1
   end 
   true
 end 
+
+[1, 5, 3]
+
 
 # def my_all?(collection)
 #   0.upto(collection.length - 1) do |idx|
